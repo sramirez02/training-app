879762ae3452de63d0c5ee285239ef85a0d3ec5d
Danitza <danitzarico1234@gmail.com>
2025-05-28 00:10:12 -0500

    agregar metricas

--- src/main/java/com/tuempresa/metrics/TrainingMetrics.java
+++ src/main/java/com/tuempresa/metrics/TrainingMetrics.java
@@ -2,8 +2,11 @@ package com.tuempresa.metrics;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class TrainingMetrics {
 
