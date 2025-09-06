fcdb22e36ba3f28776bd63a161bf9370cc8fa93c
Danitza <danitzarico1234@gmail.com>
2025-09-06 16:18:59 -0500

    Eureka funcionando

--- src/main/java/com/tuempresa/TrainingAppApplication.java
+++ src/main/java/com/tuempresa/TrainingAppApplication.java
@@ -2,11 +2,13 @@ package com.tuempresa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@ComponentScan(basePackages = "com.tuempresa")
+// @ComponentScan(basePackages = "com.tuempresa")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrainingAppApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TrainingAppApplication.class, args);
-    }
-}
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAppApplication.class, args);
+	}
+} 
